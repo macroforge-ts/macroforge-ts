a8da1d74b24c0b62765ab97f1b0937c061cad248
rymskip <17lochinskij@protonmail.com>
2025-11-23 15:33:58 -0500

    mostly error free

--- crates/swc-napi-macros/src/macros.rs
+++ crates/swc-napi-macros/src/macros.rs
@@ -1,54 +1,54 @@
-use std::fs;
-use std::path::Path;
-use swc_common::{SourceMap, sync::Lrc};
-use swc_ecma_ast::*;
-use swc_ecma_visit::{VisitMut, VisitMutWith};
+use std::{fs, path::Path};
+use swc_core::{
+    common::{DUMMY_SP, Span, SyntaxContext},
+    ecma::{
+        ast::*,
+        visit::{VisitMut, VisitMutWith},
+    },
+};
 
 pub struct MacroTransformer {
-    cm: Lrc<SourceMap>,
     filepath: String,
 }
 
 impl MacroTransformer {
-    pub fn new(cm: Lrc<SourceMap>, filepath: String) -> Self {
-        Self { cm, filepath }
+    pub fn new(filepath: String) -> Self {
+        Self { filepath }
     }
 
     /// Transform IncludeStr macro calls
     fn transform_include_str(&mut self, call: &mut CallExpr) -> Option<Expr> {
         // Check if this is an IncludeStr call
-        if let Callee::Expr(expr) = &call.callee {
-            if let Expr::Ident(ident) = &**expr {
-                if ident.sym.as_ref() == "IncludeStr" {
-                    // Get the first argument (file path)
-                    if let Some(arg) = call.args.first() {
-                        if let Expr::Lit(Lit::Str(str_lit)) = &*arg.expr {
-                            let relative_path = str_lit.value.as_ref();
+        if let Callee::Expr(expr) = &call.callee
+            && let Expr::Ident(ident) = &**expr
+            && ident.sym.as_ref() == "IncludeStr"
+        {
+            // Get the first argument (file path)
+            if let Some(arg) = call.args.first()
+                && let Expr::Lit(Lit::Str(str_lit)) = &*arg.expr
+            {
+                let relative_path = str_lit.value.to_string_lossy().into_owned();
 
-                            // Resolve path relative to the current file
-                            let current_dir =
-                                Path::new(&self.filepath).parent().unwrap_or(Path::new("."));
-                            let file_path = current_dir.join(relative_path);
+                // Resolve path relative to the current file
+                let current_dir = Path::new(&self.filepath).parent().unwrap_or(Path::new("."));
+                let file_path = current_dir.join(relative_path);
 
-                            // Read the file contents
-                            match fs::read_to_string(&file_path) {
-                                Ok(contents) => {
-                                    // Return a string literal with the file contents
-                                    return Some(Expr::Lit(Lit::Str(Str {
-                                        span: call.span,
-                                        value: contents.into(),
-                                        raw: None,
-                                    })));
-                                }
-                                Err(err) => {
-                                    eprintln!(
-                                        "Failed to read file '{}' for IncludeStr macro: {}",
-                                        file_path.display(),
-                                        err
-                                    );
-                                }
-                            }
-                        }
+                // Read the file contents
+                match fs::read_to_string(&file_path) {
+                    Ok(contents) => {
+                        // Return a string literal with the file contents
+                        return Some(Expr::Lit(Lit::Str(Str {
+                            span: call.span,
+                            value: contents.into(),
+                            raw: None,
+                        })));
+                    }
+                    Err(err) => {
+                        eprintln!(
+                            "Failed to read file '{}' for IncludeStr macro: {}",
+                            file_path.display(),
+                            err
+                        );
                     }
                 }
             }
@@ -57,93 +57,98 @@ impl MacroTransformer {
     }
 
     /// Transform @Derive decorator
-    fn transform_derive_decorator(&mut self, class: &mut Class, decorators: &mut Vec<Decorator>) {
-        let mut decorators_to_remove = vec![];
-        let mut methods_to_add = vec![];
+    fn transform_derive_decorator(&mut self, class: &mut Class) {
+        let mut generated_methods = Vec::new();
+        let mut remaining_decorators = Vec::with_capacity(class.decorators.len());
 
-        for (idx, decorator) in decorators.iter().enumerate() {
-            if let Expr::Call(call) = &*decorator.expr {
-                if let Callee::Expr(expr) = &call.callee {
-                    if let Expr::Ident(ident) = &**expr {
-                        if ident.sym.as_ref() == "Derive" {
-                            decorators_to_remove.push(idx);
-
-                            // Process each feature argument
-                            for arg in &call.args {
-                                if let Expr::Lit(Lit::Str(str_lit)) = &*arg.expr {
-                                    match str_lit.value.as_ref() {
-                                        "Debug" => {
-                                            methods_to_add.push(self.generate_to_string_method());
-                                        }
-                                        "JSON" => {
-                                            methods_to_add.push(self.generate_to_json_method());
-                                        }
-                                        _ => {}
-                                    }
-                                }
-                            }
+        for decorator in class.decorators.drain(..) {
+            if let Expr::Call(call) = decorator.expr.as_ref()
+                && let Callee::Expr(expr) = &call.callee
+                && let Expr::Ident(ident) = expr.as_ref()
+                && ident.sym.as_ref() == "Derive"
+            {
+                for arg in &call.args {
+                    if let Expr::Lit(Lit::Str(str_lit)) = arg.expr.as_ref() {
+                        let feature = str_lit.value.to_string_lossy().into_owned();
+                        match feature.as_str() {
+                            "Debug" => generated_methods.push(Self::generate_to_string_method()),
+                            "JSON" => generated_methods.push(Self::generate_to_json_method()),
+                            _ => {}
                         }
                     }
                 }
+                continue;
             }
+
+            remaining_decorators.push(decorator);
         }
 
-        // Remove processed decorators (in reverse to maintain indices)
-        for idx in decorators_to_remove.into_iter().rev() {
-            decorators.remove(idx);
-        }
-
-        // Add generated methods to the class
-        for method in methods_to_add {
-            class.body.push(method);
-        }
+        class.decorators = remaining_decorators;
+        class.body.extend(generated_methods);
     }
 
-    fn generate_to_string_method(&self) -> ClassMember {
-        let span = || Default::default();
-        let ident = |s: &str| Ident::new(s.into(), span());
-        let this_expr = || Expr::This(ThisExpr { span: span() });
+    fn span() -> Span {
+        DUMMY_SP
+    }
 
+    fn ctxt() -> SyntaxContext {
+        SyntaxContext::empty()
+    }
+
+    fn ident(name: &str) -> Ident {
+        Ident::new(name.into(), Self::span(), Self::ctxt())
+    }
+
+    fn ident_name(name: &str) -> IdentName {
+        Self::ident(name).into()
+    }
+
+    fn this_expr() -> Expr {
+        Expr::This(ThisExpr { span: Self::span() })
+    }
+
+    fn generate_to_string_method() -> ClassMember {
         // this.constructor
         let ctor_expr = Expr::Member(MemberExpr {
-            span: span(),
-            obj: Box::new(this_expr()),
-            prop: MemberProp::Ident(ident("constructor")),
+            span: Self::span(),
+            obj: Box::new(Self::this_expr()),
+            prop: MemberProp::Ident(Self::ident_name("constructor")),
         });
 
         // JSON.stringify(this)
         let stringify_expr = Expr::Call(CallExpr {
-            span: span(),
+            span: Self::span(),
+            ctxt: Self::ctxt(),
             callee: Callee::Expr(Box::new(Expr::Member(MemberExpr {
-                span: span(),
-                obj: Box::new(Expr::Ident(ident("JSON"))),
-                prop: MemberProp::Ident(ident("stringify")),
+                span: Self::span(),
+                obj: Box::new(Expr::Ident(Self::ident("JSON"))),
+                prop: MemberProp::Ident(Self::ident_name("stringify")),
             }))),
             args: vec![ExprOrSpread {
                 spread: None,
-                expr: Box::new(this_expr()),
+                expr: Box::new(Self::this_expr()),
             }],
             type_args: None,
         });
 
         let tpl = Expr::Tpl(Tpl {
-            span: span(),
+            span: Self::span(),
             exprs: vec![Box::new(ctor_expr), Box::new(stringify_expr)],
             quasis: vec![
                 TplElement {
-                    span: span(),
+                    span: Self::span(),
                     tail: false,
                     cooked: Some("".into()),
                     raw: "".into(),
                 },
                 TplElement {
-                    span: span(),
+                    span: Self::span(),
                     tail: false,
                     cooked: Some(" ".into()),
                     raw: " ".into(),
                 },
                 TplElement {
-                    span: span(),
+                    span: Self::span(),
                     tail: true,
                     cooked: Some("".into()),
                     raw: "".into(),
@@ -152,19 +157,21 @@ impl MacroTransformer {
         });
 
         let return_stmt = Stmt::Return(ReturnStmt {
-            span: span(),
+            span: Self::span(),
             arg: Some(Box::new(tpl)),
         });
 
         ClassMember::Method(ClassMethod {
-            span: span(),
-            key: PropName::Ident(ident("toString")),
+            span: Self::span(),
+            key: PropName::Ident(Self::ident_name("toString")),
             function: Box::new(Function {
                 params: vec![],
                 decorators: vec![],
-                span: span(),
+                span: Self::span(),
+                ctxt: Self::ctxt(),
                 body: Some(BlockStmt {
-                    span: span(),
+                    span: Self::span(),
+                    ctxt: Self::ctxt(),
                     stmts: vec![return_stmt],
                 }),
                 is_generator: false,
@@ -181,25 +188,25 @@ impl MacroTransformer {
         })
     }
 
-    fn generate_to_json_method(&self) -> ClassMember {
+    fn generate_to_json_method() -> ClassMember {
         ClassMember::Method(ClassMethod {
-            span: Default::default(),
-            key: PropName::Ident(Ident::new("toJSON".into(), Default::default())),
+            span: Self::span(),
+            key: PropName::Ident(Self::ident_name("toJSON")),
             function: Box::new(Function {
                 params: vec![],
                 decorators: vec![],
-                span: Default::default(),
+                span: Self::span(),
+                ctxt: Self::ctxt(),
                 body: Some(BlockStmt {
-                    span: Default::default(),
+                    span: Self::span(),
+                    ctxt: Self::ctxt(),
                     stmts: vec![Stmt::Return(ReturnStmt {
-                        span: Default::default(),
+                        span: Self::span(),
                         arg: Some(Box::new(Expr::Object(ObjectLit {
-                            span: Default::default(),
+                            span: Self::span(),
                             props: vec![PropOrSpread::Spread(SpreadElement {
-                                dot3_token: Default::default(),
-                                expr: Box::new(Expr::This(ThisExpr {
-                                    span: Default::default(),
-                                })),
+                                dot3_token: Self::span(),
+                                expr: Box::new(Self::this_expr()),
                             })],
                         }))),
                     })],
@@ -225,10 +232,10 @@ impl VisitMut for MacroTransformer {
         expr.visit_mut_children_with(self);
 
         // Transform macro calls
-        if let Expr::Call(call) = expr {
-            if let Some(new_expr) = self.transform_include_str(call) {
-                *expr = new_expr;
-            }
+        if let Expr::Call(call) = expr
+            && let Some(new_expr) = self.transform_include_str(call)
+        {
+            *expr = new_expr;
         }
     }
 
@@ -238,7 +245,7 @@ impl VisitMut for MacroTransformer {
 
         // Process decorators if present
         if !class.decorators.is_empty() {
-            self.transform_derive_decorator(class, &mut class.decorators);
+            self.transform_derive_decorator(class);
         }
     }
 
