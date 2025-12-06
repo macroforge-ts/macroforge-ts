ef6ed44b95a3697209fbe77cf08d8b5ab7e57691
rymskip <17lochinskij@protonmail.com>
2025-12-06 02:07:59 -0500

    rename

---
+++ crates/macroforge_ts/src/host/package_registry.rs
@@ -0,0 +1,16 @@
+//! Global registry for macro package registrars.
+
+use super::{MacroRegistry, Result};
+
+pub struct MacroPackageRegistration {
+    pub module: &'static str,
+    pub registrar: fn(&MacroRegistry) -> Result<()>,
+}
+
+inventory::collect!(MacroPackageRegistration);
+
+pub fn registrars() -> Vec<&'static MacroPackageRegistration> {
+    inventory::iter::<MacroPackageRegistration>
+        .into_iter()
+        .collect()
+}
